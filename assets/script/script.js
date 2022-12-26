55bd13e5dd0fcfd40d30d1bf46d3f539d7660672
Renaldas <renaldas0119@gmail.com>
2022-12-26 21:37:36 +0200

    finish

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,3 @@
-console.log('veikia');
-/*
-const firstNumber = document.getElementsByName('first');
-console.log(firstNumber[0]);
-console.log(firstNumber[0].value);
-
-
-const firstN = firstNumber[0];
-console.log(firstN);
-console.log(firstN.value);
-*/
 
 
 // PIRMO SKAIČIAUS LANGELIO VERTĖS PAĖMIMAS, TIKRINIMAS
@@ -17,10 +6,13 @@ const firstNumber = document.querySelector('.first');
 console.log(firstNumber);
 console.log(firstNumber.value);
 
+const buttonBasic = document.querySelector('.buttonBasic');
+const results = document.querySelector('.resultsBasic')
 const error1 = document.querySelector('.error1')
 
 let check1 = 0;
 let check2 = 0;
+let check3 = 0;
 
 function numberCheck(){
 let skai = firstNumber.value
@@ -39,28 +31,26 @@ if (count == a && a != 0) {
     console.log(`Pirmo skaiciai teisingi`);
     error1.innerHTML ='';
     check1 = 1;
-    return check1;
+    
 } 
 if (count != a) {
     // console.log(`Rašykite tik skaičius`);
     error1.innerHTML ='*Rašykite tik skaičius';
     check1 = 0;
-    return check1;
 }
 if (a == 0) {
     error1.innerHTML ='';
     check1 = 0;
-    return check1;
-}console.log(check1, check2);
+} return check1;
 }
 
 firstNumber.addEventListener('keyup', numberCheck);
 
 
+
 // ANTRO SKAIČIAUS LANGELIO VERTĖS PAĖMIMAS, TIKRINIMAS
 
 const secondNumber = document.querySelector('.second');
-
 const error3 = document.querySelector('.error3')
 
 function numberCheck2(){
@@ -79,32 +69,25 @@ function numberCheck2(){
     if (count == a && a != 0) {
         console.log(`Antro skaiciai teisingi`);
         error3.innerHTML ='';
-        check2 = 1;
-        return check2;
+        check3 = 1;
+       
     } 
     if (count != a) {
         // console.log(`Rašykite tik skaičius`);
         error3.innerHTML ='*Rašykite tik skaičius';
-        check2 = 0;
-        return check2;
+        check3 = 0;
     }
     if (a == 0) {
         error3.innerHTML ='';
-        check2 = 0;
-        return check2;
-    }console.log(check1, check2);
+        check3 = 0;
+    } return check3;
 }
     
 secondNumber.addEventListener('keyup', numberCheck2);
 
-check = check1 + check2;
-console.log(check1, check2);
+
 
 // VEIKSMO VERTĖS PAĖMIMAS
-
-
-
-
 
 
 const mainForm = document.forms.main;
@@ -113,7 +96,9 @@ const mainForm = document.forms.main;
 const sarasas = mainForm.nameSelect;
 // console.log(sarasas.options);
 
+
 function veiksmas(){
+
 // gaunam indeksa parinkto option
 const index = sarasas.selectedIndex
 console.log(index);
@@ -127,72 +112,85 @@ selectedVerte = verte1;
 
 return selectedIndex, selectedVerte;
 }
-console.log(veiksmas());
 
 mainForm.addEventListener('click', veiksmas);
 
 
 // REZULTATO SKAIČIAVIMAS (MYGTUKO PASPAUDIMAS)
 
-const button = document.querySelector('.button');
-console.log(button);
+const button = document.getElementById('itemID');
+    console.log(button);
+
 
 function calc(){
-    // let a = +firstNumber.value;
-    // let b = verte;
-    // let c = +secondNumber.value;
-    // console.log(a);
+        
 let indeksas = selectedIndex;
 let verte = selectedVerte;
+
+if(indeksas == 0){
+    a = +firstNumber.value * +secondNumber.value;
+}
+
 if(indeksas == 1){
     a = +firstNumber.value * +secondNumber.value;
-    // console.log(`${+firstNumber.value} ${verte} ${+secondNumber.value} = ${a}`);
-    // return `${+firstNumber.value} ${verte} ${+secondNumber.value} = ${a}`;
 }
+
 if(indeksas == 2){
     a = +firstNumber.value / +secondNumber.value;
-    // return firstNumber.value;
-    // console.log(+firstNumber.value +verte +secondNumber.value);
-    // console.log(+firstNumber.value);
-    // console.log(typeof +firstNumber.value);
-    // console.log(verte);
-    // console.log(typeof +verte);
-    // console.log(+secondNumber.value);
-    // console.log('5599');
 }
+
 if(indeksas == 3){
     a = +firstNumber.value + +secondNumber.value;
 }
+
 if(indeksas == 4){
     a = +firstNumber.value - +secondNumber.value;
 }
 
+// REZULTATO ĮTERPIMAS
     const results = document.querySelector('.results');
     const newElement = document.createElement('div');
-    newElement.innerHTML = `${+firstNumber.value} ${verte} ${+secondNumber.value} = ${a}`;
-    results.prepend(newElement);
+    // newElement.innerHTML = `${+firstNumber.value} ${verte} ${+secondNumber.value} = ${a}`;
+    newElement.innerHTML = `${+firstNumber.value} ${verte} ${+secondNumber.value} = ${(Math.round(((a))*(10**10))/(10**10))}`;
+    // newElement.innerHTML = (Math.round(((0.121321 * 0.21245)+Number.EPSILON)*(10 ** 100))/(10 ** 100));
+    // newElement.innerHTML = (Math.round(((0.121321 * 0.21245)+Number.EPSILON)*(10 ** 100))/(10 ** 100));
+    // newElement.innerHTML = (Math.round(((a))*(10**10))/(10**10));
 
-    console.log(veiksmas());
+    results.prepend(newElement);
+     
 }
-// console.log(calc())
 
 button.addEventListener('click', calc);
 
 
 
-
-
-
-// REZULTATO ĮTERPIMAS
-/*
-    const results = document.querySelector('.results');
-    const newElement = document.createElement('div');
-    newElement.innerHTML = calc();
-    results.prepend(newElement);
-
-*/
-
-
 // MYGTUKO AKTYVAVIMAS
 
+const inputArea = document.querySelector('.inputArea');
 
+function cheking(){
+
+if(selectedIndex == 0){
+    check2 = 0;
+} else check2 = 1;
+
+
+    checkSum = check1 + check2 + check3;
+    console.log(checkSum)
+
+    if(checkSum < 3){
+        buttonBasic.classList.remove('enabledButton');
+        buttonBasic.classList.add('disabledButton');
+        results.classList.remove('results');
+    }
+    if(checkSum == 3){
+        buttonBasic.classList.add('enabledButton');
+        buttonBasic.classList.remove('disabledButton');
+        results.classList.add('results');
+    }
+   
+}
+
+inputArea.addEventListener('click', cheking);
+inputArea.addEventListener('mouseover', cheking);
+inputArea.addEventListener('keyup', cheking);
